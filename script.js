7667bf2610af2101d42c68dce92009a6d18d93a1
Karan vishwakarma <150724533+Karan09542@users.noreply.github.com>
2025-11-03 21:45:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,16 +4,38 @@ class EventTarget {
     }
   
     addEventListener(event, callback) {
-      
+		const callArr = this.listeners.get(event) || [];
+		
+		if(!callback.name){
+			Object.defineProperty(callback, "name", {
+				value: `${event}-${callArr.length}`,
+				configurable: true,
+			})
+		}
+		callArr.push(callback)
+		this.listeners.set(event, callArr)      
     }
   
     removeEventListener(event, callback) {
-      
+      const callArr = this.listeners.get(event) || [];
+	  this.listeners.set(event, callArr.filter(call => call.name !== callback.name));  
     }
   
     dispatchEvent(event) {
-    
+		const callArr = this.listeners.get(event) || [];
+	    for(let callback of callArr){
+			callback()
+		}
     }
     
   }
+
+const target = new EventTarget();
+function ram(){
+	console.log("radheshyam")
+}
+target.addEventListener("strong", () => {console.log("ram")})
+target.addEventListener("strong", () => {console.log("sitaram")})
+target.addEventListener("sdv", ram)
+
   
